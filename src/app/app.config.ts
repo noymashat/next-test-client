3f6700615fa3930b97a967b828b05d4c455182f4
noymashat <34368457+noymashat@users.noreply.github.com>
2023-12-13 17:47:02 +0200

    revert unwanted changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { MovieListComponent } from './components/movie-list/movie-list.component';
-import { moviesResolver } from './services/movies.resolver';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter([
-      {
-        path: '',
-        component: MovieListComponent,
-        resolve: { movies: moviesResolver }
-      }
-    ]),
+    provideRouter(routes),
     provideHttpClient()
   ]
 };
